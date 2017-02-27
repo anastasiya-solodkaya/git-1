a60d3b4f8bb12099250c83422ee7d78afe48d009
Anastasiya Solodkaya <a.s.solodkaya@gmail.com>
2017-02-28 02:42:58 +0300

    Два java-файла

---
+++ src/com/levelp/jb2/HelloWorld2.java
@@ -0,0 +1,10 @@
+package com.levelp.jb2;
+
+/**
+ * Created by asolodkaya on 28.02.17.
+ */
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World 2!");
+    }
+}
